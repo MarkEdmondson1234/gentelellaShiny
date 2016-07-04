fa578eb20f0189755839cbbb78b710684630e69a
Mark IIH <mark@iihnordic.com>
2016-07-04 10:51:46 +0200

    hardcode shiny port in example to 1221

--- R/example.R
+++ R/example.R
@@ -5,6 +5,7 @@ runExample <- function() {
     stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
   }
 
+  options(shiny.port = 1221)
   browseURL(system.file("www", "login.html", package = "gentelellaShiny"))
   shiny::runApp(appDir, display.mode = "normal")
 
