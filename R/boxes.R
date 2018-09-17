1fa772c23940756883a2c1f0aeb3e7ddb34f039f
Divad0175 <dgranjon@ymail.com>
2018-09-17 22:26:03 +0200

    add tabSetPill (vertical tabPanels)

--- R/boxes.R
+++ R/boxes.R
@@ -416,3 +416,131 @@ gentelellaTabPanel <- function(..., tabName, active = FALSE) {
     ...
   )
 }
+
+
+
+#' Create a gentelella tabSetPill
+#'
+#'
+#' @param ... Slot for gentelellaTabPill.
+#' @param id TabSetPanel id. Should be unique.
+#' @param right If TabSetPanel start from the right side. FALSE by default.
+#'
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @examples
+#' if (interactive()) {
+#'  library(shiny)
+#'  library(gentelellaShiny)
+#'  shinyApp(
+#'   ui = gentelellaPage(
+#'    gentelellaBody(
+#'     gentelellaTabSetPill(
+#'      gentelellaTabPill(
+#'        tabName = "Home",
+#'        active = TRUE,
+#'        "Raw denim you probably haven't heard of
+#'        them jean shorts Austin. Nesciunt tofu stumptown
+#'        aliqua, retro synth master cleanse. Mustache
+#'        cliche tempor, williamsburg carles vegan helvetica.
+#'        Reprehenderit butcher retro keffiyeh dreamcatcher synth.
+#'        Cosby sweater eu banh mi, qui irure terr."
+#'      ),
+#'      gentelellaTabPill(
+#'        tabName = "Profile",
+#'        active = FALSE,
+#'        sliderInput(
+#'          "obs",
+#'          "Number of observations:",
+#'          min = 0,
+#'          max = 1000,
+#'          value = 500
+#'        ),
+#'        plotOutput("distPlot")
+#'      )
+#'     )
+#'    )
+#'   ),
+#'   server = function(input, output, session) {
+#'    output$distPlot <- renderPlot({
+#'     hist(rnorm(input$obs))
+#'    })
+#'   }
+#'  )
+#' }
+#'
+#' @export
+gentelellaTabSetPill <- function(..., right = FALSE) {
+
+  tabSetCl <- if (right) {
+    "nav nav-tabs tabs-right"
+  } else {
+    "nav nav-tabs tabs-left"
+  }
+
+  tabItems <- list(...)
+  len_items <- length(tabItems)
+
+  tabMenu <- lapply(X = 1:len_items, FUN = function(i) {
+    current_item <- tabItems[[i]]
+    current_item_cl <- current_item$attribs$class
+    current_item_name <- current_item$attribs$id
+    active <- sum(grep(x = current_item_cl, pattern = "active")) == 1
+    shiny::tags$li(
+      class = if (active == 1) "active" else NA,
+      shiny::tags$a(
+        href = paste0("#", current_item_name),
+        `data-toggle` = "tab",
+        `aria-expanded` = if (active == 1) "true" else "false",
+        current_item_name
+      )
+    )
+  })
+
+  menuTag <- shiny::tags$ul(
+    class = tabSetCl,
+    tabMenu
+  )
+
+  contentTag <- shiny::tags$div(
+    class = "tab-content",
+    ...
+  )
+
+  mainTag <- if (right) {
+    shiny::tagList(
+      shiny::tags$div(class = "col-xs-9", contentTag),
+      shiny::tags$div(class = "col-xs-3", menuTag)
+    )
+  } else {
+    shiny::tagList(
+      shiny::tags$div(class = "col-xs-3", menuTag),
+      shiny::tags$div(class = "col-xs-9", contentTag)
+    )
+  }
+  mainTag
+}
+
+
+
+#' Create a gentelella tabPill
+#'
+#' To be included in a gentelellaTabSetPill
+#'
+#' @param ... Tab content
+#' @param tabName Tab name: it will be also passed as the id argument. Should be unique.
+#' @param active Whether the tab is active or not. FALSE bu default.
+#'
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @export
+gentelellaTabPill <- function(..., tabName, active = FALSE) {
+
+  tabCl <- if (active) "tab-pane active" else "tab-pane"
+
+  shiny::tags$div(
+    class = tabCl,
+    id = tabName,
+    ...
+  )
+}
