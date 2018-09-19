fcbda55b29f15f8afc609ad54cb3f680a2d5d230
Divad0175 <dgranjon@ymail.com>
2018-09-19 16:53:45 +0200

    fix gentelellaGallery shiny port issue

--- R/gentelellaGallery.R
+++ R/gentelellaGallery.R
@@ -12,13 +12,12 @@ gentelellaGallery <- function() {
   if (!requireNamespace(package = "gentelellaShiny"))
     message("Package 'gentelellaShiny' is required to run this function")
 
-  options(shiny.port = 1221)
   #browseURL(system.file("www", "login.html", package = "gentelellaShiny"))
   shiny::shinyAppFile(
     system.file(
       paste0("example/app.R"),
-      package = 'gentelellaShiny',
-      mustWork = TRUE
+      package = 'gentelellaShiny'#,
+      #mustWork = TRUE
     )
   )
 
